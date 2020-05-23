cd99776f53e020a812c157f526fa8a6dd65be928
Alan <alanvega002@gmail.com>
2020-05-22 16:14:53 -0800

    DAO created

--- src/daos/sqlite.ts
+++ src/daos/sqlite.ts
@@ -1,130 +1,128 @@
 import * as sqlite3 from "sqlite3";
 import * as path from "path";
-import Promise, { reject } from "bluebird";
+import * as promise from "bluebird";
 
+const Promise = promise.Promise;
 const sqlite = sqlite3.verbose();
 const dbPath: string = path.resolve(__dirname, "sqlite.db");
-const database = new sqlite.Database(dbPath, (err) => {
-  if (err) {
-    return console.error("FAILED DATABASE CONNECTION:", err);
-  }
-  console.log("Conencted to database.");
-});
 
 // Creates DAO for address database
-class DB {
+class DAO {
   db: sqlite3.Database;
   constructor() {
-    this.db = await new sqlite.Database(dbPath, (err) => {
+    this.db = new sqlite.Database(dbPath, (err) => {
       if (err) {
-        return console.error("FAILED DATABASE CONNECTION:", err);
+        console.log("Could not connect to database", err);
+      } else {
+        console.log("Connected to database");
       }
-      console.log("Conencted to database.");
-    });;
-  }
-
-  async createTable() {
-    let sql = `CREATE TABLE IF NOT EXISTS addresses(
-      id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-      address text NOT NULL UNIQUE,
-      amount integer NOT NULL DEFAULT 0)`;
-    return await this.db.run(sql, (err) => {
-      if (err) {
-        return console.error("TABLE CREATION ERROR", err.message);
-      }
-      console.log("Address table created.");
     });
   }
 
-  dataUpdate(
-    addresses: string[] | null,
+  run(sql: string, params: any) {
+    return new Promise((resolve, reject) => {
+      this.db.run(sql, params, function (err) {
+        if (err) {
+          console.log("Error running sql " + sql);
+          console.log(err);
+          reject(err);
+        } else {
+          resolve(console.log("Resolved"));
+        }
+      });
+    });
+  }
+
+  update(
+    addresses: [string] | null,
     singleAddress: string | null,
     amount: number | null
   ) {
-    // addresses = addresses || null;
-    // singleAddress = singleAddress || null;
-    // amount = amount || null;
-
     // Ethereum address row insertion
     if (addresses) {
       let addressPlaceholders = addresses.map((address) => "(?)").join(",");
       let addressSQL =
         `INSERT INTO addresses (address) VALUES ` + addressPlaceholders;
-
-      return new Promise ((resolve, reject) => {
-        this.db.run(addressSQL, addresses, (err) => {
-          if (err) {
-            console.log(err.message);
-            reject(err)
-          }
-          resolve(console.log(`Address inserted: ${addressPlaceholders.length}`));
-      });
-    }}
+      console.log("Updated:", addresses);
+      return this.run(addressSQL, addresses);
+    }
 
     if (singleAddress && amount) {
       let amountSQL = `
-        UPDATE addresses
-          SET amount = ?
-          WHERE address = ?`;
-      return new Promise((resolve, reject) => {return this.db.run(amountSQL, [singleAddress, amount], (err) => {
-        if (err) {
-          return console.error("ERROR ENTERING AMOUNT:", err.message);
-        }
-        console.log(`UPDATED: ${singleAddress}, ${amount}`);
-      })});
+      UPDATE addresses
+        SET amount = ?
+        WHERE address = ?`;
+      let data = [singleAddress, amount];
+      console.log("Updated:", data);
+      return this.run(amountSQL, data);
     }
   }
 
-  async delete(address: string) {
+  delete(address: string) {
     let addressDelete = ` DELETE FROM addresses WHERE address = ?`;
 
-    return await this.db.run(addressDelete, [address], (err) => {
-      if (err) {
-        return console.error("ERROR DELETING ADDRESS:", err.message);
-      }
-      console.log("Deleted:", address);
+    console.log("Deleted:", address);
+    return this.run(addressDelete, [address]);
+  }
+
+  createTable() {
+    let sql = `CREATE TABLE IF NOT EXISTS addresses(
+      id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+      address text NOT NULL UNIQUE,
+      amount integer NOT NULL DEFAULT 0)`;
+    console.log("Table created");
+    return this.run(sql, []);
+  }
+
+  get(address: string) {
+    let getAddressSQL = `SELECT * FROM addresses WHERE address = ?`;
+    return new Promise((resolve, reject) => {
+      this.db.get(getAddressSQL, [address], (err, result) => {
+        if (err) {
+          console.log("Error running sql: " + address);
+          console.log(err);
+          reject(err);
+        } else {
+          console.log(result);
+          resolve(result);
+        }
+      });
     });
   }
 
-  async get(address: string) {
-    let getAddressSQL = `SELECT * FROM addresses WHERE address = ?`;
-    let output;
-    return (output = await this.db.get(
-      getAddressSQL,
-      [address],
-      (err, results) => {
-        if (err) {
-          return console.error("ERROR GETTING ADDRESS DATA:", err.message);
-        }
-        output = results;
-        console.log(output);
-        // return res;
-      }
-    ));
-    // return output;
-  }
-
-  async all() {
+  all() {
     let allDataSQL = `SELECT address amount FROM addresses`;
 
-    return await this.db.all(allDataSQL, (err, results) => {
-      if (err) {
-        return console.error("ERROR GETTING ALL DATA:", err.message);
-      }
-      return console.log(results);
+    return new Promise((resolve, reject) => {
+      this.db.all(allDataSQL, (err, rows) => {
+        if (err) {
+          console.log("Error running sql: " + allDataSQL);
+          console.log(err);
+          reject(err);
+        } else {
+          resolve(rows);
+
+          close() {
+            return this.db.close((err) => {
+              if (err) {
+                return console.error("Error closign database:", err);
+              }
+              console.log("Database closed.");
+            });
+          }
+        }
+      });
     });
   }
 
-  async close() {
-    return await this.db.close((err) => {
+  close() {
+    return this.db.close((err) => {
       if (err) {
-        return console.error(`ERROR CLOSING DATABASE:`, err.message);
+        return console.error("Error closign database:", err);
       }
       console.log("Database closed.");
     });
   }
 }
 
-const test = new DB();
-test.get("test");
-// test.all();
+export default DAO;
--- src/etherscan/addresses.ts
+++ src/etherscan/addresses.ts
@@ -1,1 +1,13 @@
 import axios from "axios";
+import * as promise from "bluebird";
+import dotenv from "dotenv";
+import DAO from "../daos/sqlite";
+
+dotenv.config({ path: "../.env" });
+const Promise = promise.Promise;
+
+export default function getAddresses() {
+  let addressList: [] = [];
+
+  return new Promise((resolve, reject) => {});
+}
